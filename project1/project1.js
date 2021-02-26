c7fed790a7a340fbe2c70ed541c7e8ab07cae3d9
vinayreddie <vinayreddhek@gmail.com>
2021-02-26 11:09:56 -0500

    .

--- project1/project1.js
+++ project1/project1.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-    $("#thumbBox").click(function(e) {
+    $("#thumbBox").click(function(event) {
         if (event.target && event.target.matches('img')) {
             var thumbImg = event.target.cloneNode();
             thumbImg.src = thumbImg.src.replace('small', 'medium');
@@ -9,14 +9,11 @@ $(document).ready(function() {
             applyFilters();
         }
     });
-
-
     $('#sliderOpacity').on('input', function(event) {
         $('#sliderOpacity').attr('value', event.target.value);
         $('#numOpacity').html(event.target.value);
         applyFilters();
     });
-
     $('#sliderSaturation').on('input', function(event) {
         $('#sliderSaturation').attr('value', event.target.value);
         var val = event.target.value;
@@ -24,7 +21,6 @@ $(document).ready(function() {
         $('#numSaturation').html(val);
         applyFilters();
     });
-
     $('#sliderBrightness').on('input', function(event) {
         $('#sliderBrightness').attr('value', event.target.value);
         let val = event.target.value;
@@ -32,27 +28,23 @@ $(document).ready(function() {
         $('#numBrightness').html(val);
         applyFilters();
     });
-
     $('#sliderHue').on('input', function(event) {
         $('#sliderHue').attr('value', event.target.value);
         $('#numHue').html(event.target.value);
         applyFilters();
     });
-
     $('#sliderGray').on('input', function(event) {
         $('#sliderGray').attr('value', event.target.value);
         $('#numGray').html(event.target.value);
         applyFilters();
     });
-
     $('#sliderBlur').on('input', function(event) {
         $('#sliderBlur').attr('value', event.target.value);
         $('#numBlur').html(event.target.value);
         applyFilters();
     });
-
     var applyFilters = function() {
-        let filters = 'opacity(' + $('#sliderOpacity').attr('value') + '%)' +
+        var filters = 'opacity(' + $('#sliderOpacity').attr('value') + '%)' +
             'brightness(' + $('#sliderBrightness').attr('value') + '%) ' +
             'saturate(' + $('#sliderSaturation').attr('value') + '%) ' +
             'grayscale(' + $('#sliderGray').attr('value') + '%) ' +
@@ -61,10 +53,7 @@ $(document).ready(function() {
 
         $('#imgManipulated>img').css('filter', filters);
 
-
-
     }
-
     $('#resetFilters').on('click', function() {
         $('#imgManipulated>img').css('filter',
             'opacity(100%) ' +
