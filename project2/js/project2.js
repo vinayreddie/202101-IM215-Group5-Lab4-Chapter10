37b908fa8e1d4f8b831086fb02ca3e44be49f5c9
vinayreddie <vinayreddhek@gmail.com>
2021-02-26 23:41:11 -0500

    Final

--- project2/js/project2.js
+++ project2/js/project2.js
@@ -5,22 +5,23 @@ $(document).ready(function() {
     });
     $("img").mouseenter(function() {
         $(this).addClass("gray");
-        var di = $("<div></div>").attr('id', 'preview');
-        var image = $("<img></img>").attr('src', $(this).attr('src').replace('square', 'medium')).attr('alt', $(this).attr('alt'));
-        div.append(image);
+        var div = $("<div></div>").attr('id', 'preview');
+        var img = $("<img></img>").attr('src', $(this).attr('src').replace('square', 'medium')).attr('alt', $(this).attr('alt'));
+        div.append(img);
         images.forEach(ele => {
             if (ele.title == $(this).attr('alt')) {
-                var p = $("<p></p>").html(ele.title + " " + ele.city + ", " + ele.country + " [" + ele.taken + "]");
+                var p = $("<p></p>").html(ele.title + "</br>" + ele.city + ", " + ele.country + " [" + ele.taken + "]");
                 div.append(p);
             }
         });
-        $("body").append(di);
-    });
-    $("img").mousemove(function(event) {
-        $("#preview").css('left', event.pageX).css('top', event.pageY);
-    });
+        $("#preview").css('left', event.pageX + 10).css('top', event.pageY + 10);
+        $("body").append(div);
+    })
     $("img").mouseleave(function() {
         $("#preview").remove();
         $(this).removeClass("gray");
+    })
+    $("img").mousemove(function(e) {
+        $('#preview').css("display", "block").css('left', e.pageX + 20).css('top', e.pageY + 10);
     });
 });
